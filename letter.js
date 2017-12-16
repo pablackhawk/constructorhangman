9aa3a50a08d60cfd31330d98b4317a495fcf637e
pablackhawk <l.tirtarahardja@gmail.com>
2017-12-16 13:56:19 -0800

    fixed variables

--- letter.js
+++ letter.js
@@ -1,10 +1,12 @@
 // Stores letters
 var Letter = function (letter) {
   this.letter = letter
+  // Checks to see if letter can be displayed
   this.appear = false
 
   this.letterShow = function () {
     if (this.letter === ' ') {
+      // Error checks to make sure blank not read as false
       this.appear = true
       return ' '
     } else if (this.appear === false) {
